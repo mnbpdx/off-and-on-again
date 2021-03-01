a5ea8cfd4a797019764411c2993ea0031a3a5763
Mark N. Beebe <mbeebe08@gmail.com>
2021-02-28 18:46:14 -0800

    Switching machines, working on client

--- switch-client/SwitchMonitor.py
+++ switch-client/SwitchMonitor.py
@@ -2,28 +2,46 @@ import requests
 from gpiozero import Button
 
 def main():
+    url = 'http://localhost:5000/switches' 
+
     print("Monitoring Beaver Tail Switch...")
 
     beaverTail = Button(2)
 
     while True:
         printValues()
-        beaverTail.wait_for_press()
-        print("Beaver tail switched on!")
+        waitForSwitchOn(url)
         printValues()
-        beaverTail.wait_for_release()
-        print("Beaver tail switched off!")
-
-def htmlTest():
-    payload = {'name': 'ACoolSwitch', 'status': '1', 'countFlips': '8'}
-    r = requests.post('http://localhost:5000/switches', json=payload)
-    print(r.text)
+        waitForSwitchOff(url)
 
 def printValues():
     print("Current Values of beaverTail object:")
     print("is_held: " + str(beaverTail.is_held))
+    print("is_pressed: " + str(beaverTail.is_pressed))
+    print("held_time: " + str(beaverTail.held_time))
+    print("hold_repeat: " + str(beaverTail.hold_repeat))
+    print("hold_time: " + str(beaverTail.hold_time))
+    print("is_pressed: " + str(beaverTail.is_held))
+    print("is_pressed: " + str(beaverTail.is_held))
+    print("is_pressed: " + str(beaverTail.is_held))
     print("is_pressed: " + str(beaverTail.is_held))
 
 
-htmlTest()
-#main()
+def waitForSwitchOn(url):
+    beaverTail.wait_for_press()
+    r = requests.patch(url, json=payload)
+    print("Beaver Tail switched on!")
+    
+def waitForSwitchOff(url):
+    beaverTail.wait_for_release()
+    r = requests.patch(url, json=payload)
+    print("Beaver Tail switched off!")
+
+main()
+
+
+#def htmlTest():
+#    payload = {'name': 'A New Switch', 'status': '1', 'countFlips': '9001'}
+#    r = requests.post(url, json=payload)
+#    print(r.text)
+
